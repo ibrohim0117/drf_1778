31cdd3d61b2292fb64c4ba5eab0e6d49bf972b61
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2024-10-03 10:40:02 +0500

    .

--- drf_18/settings.py
+++ drf_18/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from decouple import config
 from pathlib import Path
 
@@ -106,6 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
